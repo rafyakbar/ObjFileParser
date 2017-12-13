c8ae15e965e73274958461e298ccc4aa9116fd50
Rafy Aulia Akbar <rafy683@gmail.com>
2017-12-13 15:47:56 +0700

    first

---
+++ ObjFileParser/GUI.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace ObjFileParser
+{
+    partial class GUI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(GUI));
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.browseButton = new System.Windows.Forms.Button();
+            this.infoTextBox = new System.Windows.Forms.TextBox();
+            this.glNormal3f = new System.Windows.Forms.CheckBox();
+            this.glTexCoord3f = new System.Windows.Forms.CheckBox();
+            this.parseButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(12, 70);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(423, 23);
+            this.progressBar.TabIndex = 5;
+            // 
+            // browseButton
+            // 
+            this.browseButton.Location = new System.Drawing.Point(361, 12);
+            this.browseButton.Name = "browseButton";
+            this.browseButton.Size = new System.Drawing.Size(75, 23);
+            this.browseButton.TabIndex = 4;
+            this.browseButton.Text = "Pilih file .obj";
+            this.browseButton.UseVisualStyleBackColor = true;
+            this.browseButton.Click += new System.EventHandler(this.browseButton_Click);
+            // 
+            // infoTextBox
+            // 
+            this.infoTextBox.Location = new System.Drawing.Point(13, 14);
+            this.infoTextBox.Name = "infoTextBox";
+            this.infoTextBox.Size = new System.Drawing.Size(350, 20);
+            this.infoTextBox.TabIndex = 3;
+            // 
+            // glNormal3f
+            // 
+            this.glNormal3f.AutoSize = true;
+            this.glNormal3f.Checked = true;
+            this.glNormal3f.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.glNormal3f.Location = new System.Drawing.Point(99, 45);
+            this.glNormal3f.Name = "glNormal3f";
+            this.glNormal3f.Size = new System.Drawing.Size(67, 17);
+            this.glNormal3f.TabIndex = 6;
+            this.glNormal3f.Text = "glNormal";
+            this.glNormal3f.UseVisualStyleBackColor = true;
+            // 
+            // glTexCoord3f
+            // 
+            this.glTexCoord3f.AutoSize = true;
+            this.glTexCoord3f.Checked = true;
+            this.glTexCoord3f.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.glTexCoord3f.Location = new System.Drawing.Point(13, 45);
+            this.glTexCoord3f.Name = "glTexCoord3f";
+            this.glTexCoord3f.Size = new System.Drawing.Size(80, 17);
+            this.glTexCoord3f.TabIndex = 7;
+            this.glTexCoord3f.Text = "glTexCoord";
+            this.glTexCoord3f.UseVisualStyleBackColor = true;
+            // 
+            // parseButton
+            // 
+            this.parseButton.Location = new System.Drawing.Point(361, 41);
+            this.parseButton.Name = "parseButton";
+            this.parseButton.Size = new System.Drawing.Size(75, 23);
+            this.parseButton.TabIndex = 8;
+            this.parseButton.Text = "Parse";
+            this.parseButton.UseVisualStyleBackColor = true;
+            this.parseButton.Click += new System.EventHandler(this.parseButton_Click);
+            // 
+            // GUI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ClientSize = new System.Drawing.Size(448, 104);
+            this.Controls.Add(this.parseButton);
+            this.Controls.Add(this.glTexCoord3f);
+            this.Controls.Add(this.glNormal3f);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.browseButton);
+            this.Controls.Add(this.infoTextBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "GUI";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = ".obj file parser (by Rafy AA & Respita YR)";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.Button browseButton;
+        private System.Windows.Forms.TextBox infoTextBox;
+        private System.Windows.Forms.CheckBox glNormal3f;
+        private System.Windows.Forms.CheckBox glTexCoord3f;
+        private System.Windows.Forms.Button parseButton;
+    }
+}
+
---
+++ ObjFileParser/GUI.cs
@@ -0,0 +1,242 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace ObjFileParser
+{
+    public partial class GUI : Form
+    {
+        List<string> rows;
+
+        List<string> vertices;
+        List<string> tex_coords;
+        List<string> normals;
+        List<string> faces;
+
+        //bug inisialisasi array faces pada c++
+        //faces[][inisisalisasi ini yg ngebug, belum dihitung][3]
+
+        string file_name;
+
+        public GUI()
+        {
+            InitializeComponent();
+        }
+
+        // mengambil data obj dari user dan mengembalikannya
+        private List<string> browse()
+        {
+            try
+            {
+                OpenFileDialog chooseFile = new OpenFileDialog();
+
+                chooseFile.InitialDirectory = "D:\\";
+                chooseFile.Filter = "obj files (*.obj)|*.obj";
+                chooseFile.FilterIndex = 1;
+                chooseFile.RestoreDirectory = true;
+                chooseFile.Multiselect = false;
+
+                if (chooseFile.ShowDialog() == DialogResult.OK)
+                {
+                    infoTextBox.Text = chooseFile.FileName;
+                    file_name = chooseFile.SafeFileName.ToLower().Replace(' ', '_').Replace(".obj", "");
+                    try
+                    {
+                        return File.ReadAllLines(chooseFile.FileName).ToList();
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.ToString());
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("ngebug :v -> " + ex.ToString());
+            }
+
+            return null;
+        }
+
+        private void browseButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                rows = browse();
+                // inisialisasi awal
+                vertices = new List<string>();
+                tex_coords = new List<string>();
+                normals = new List<string>();
+                faces = rows.Where(i => i.Split(' ')[0] == "f").ToList();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("ngebug :v -> " + ex.ToString());
+            }
+        }
+
+        private void parseButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // menghitung progressbar max progressbar
+                progressBar.Maximum = faces.Count + rows.Where(i => i.Split(' ')[0] == "v").ToList().Count + 1;
+                progressBar.Step = 1;
+                progressBar.Value = 0;
+                if (glTexCoord3f.Checked)
+                {
+                    progressBar.Maximum += rows.Where(i => i.Split(' ')[0] == "vt").ToList().Count;
+                }
+                if (glNormal3f.Checked)
+                {
+                    progressBar.Maximum += rows.Where(i => i.Split(' ')[0] == "vn").ToList().Count;
+                }
+
+                string result = "void " + file_name + "() {" + Environment.NewLine;
+                List<string> dumpList;
+
+                dumpList = rows.Where(i => i.Split(' ')[0] == "v").ToList();
+                result += "\tfloat vertices[][3] = {" + Environment.NewLine;
+                foreach (string vertex in dumpList)
+                {
+                    string dump = vertex.Replace("   ", " ").Replace("  ", " ").Replace("v ", "");
+                    dump = (dump[dump.Length - 1] == ' ') ? dump.Remove(dump.Length - 1) : dump;
+                    string[] column = dump.Split(' ');
+
+                    result += "\t\t{" + column[0] + ", " + column[1] + ", " + column[2] + "}," + Environment.NewLine;
+
+                    progressBar.Value++;
+                }
+                result += "\t};" + Environment.NewLine;
+
+                // menambahkan texcoord dicentang 
+                if (glTexCoord3f.Checked)
+                {
+                    dumpList = rows.Where(i => i.Split(' ')[0] == "vt").ToList();
+                    result += "\tfloat tex_coords[][3] = {" + Environment.NewLine;
+                    foreach (string coord in dumpList)
+                    {
+                        string dump = coord.Replace("   ", " ").Replace("  ", " ").Replace("vt ", "");
+                        dump = (dump[dump.Length - 1] == ' ') ? dump.Remove(dump.Length - 1) : dump;
+                        string[] column = dump.Split(' ');
+
+                        if (column.Length == 3)
+                        {
+                            result += "\t\t{" + column[0] + ", " + column[1] + ", " + column[2] + "}," + Environment.NewLine;
+                        }
+                        else
+                        {
+                            result += "\t\t{" + column[0] + ", " + column[1] + "}," + Environment.NewLine;
+                        }
+
+                        progressBar.Value++;
+                    }
+                    result += "\t};" + Environment.NewLine;
+                }
+
+                //menambahkan normal jika dicentang
+                if (glNormal3f.Checked)
+                {
+                    dumpList = rows.Where(i => i.Split(' ')[0] == "vn").ToList();
+                    result += "\tfloat normals[][3] = {" + Environment.NewLine;
+                    foreach (string normal in dumpList)
+                    {
+                        string dump = normal.Replace("   ", " ").Replace("  ", " ").Replace("vn ", "");
+                        dump = (dump[dump.Length - 1] == ' ') ? dump.Remove(dump.Length - 1) : dump;
+                        string[] column = dump.Split(' ');
+
+                        if (column.Length == 3)
+                        {
+                            result += "\t\t{" + column[0] + ", " + column[1] + ", " + column[2] + "}," + Environment.NewLine;
+                        }
+                        else
+                        {
+                            result += "\t\t{" + column[0] + ", " + column[1] + "}," + Environment.NewLine;
+                        }
+
+                        progressBar.Value++;
+                    }
+                    result += "\t};" + Environment.NewLine;
+                }
+
+                dumpList = rows.Where(i => i.Split(' ')[0] == "f").ToList();
+                int max = dumpList.OrderBy(i => i.Split(' ').Length).ToList()[0].Split(' ').Length;
+                result += "\tint faces[][" + max + "][3] = {" + Environment.NewLine;
+                foreach (string face in dumpList)
+                {
+                    string dump = face.Replace("   ", " ").Replace("  ", " ").Replace("f ", "");
+                    dump = (dump[dump.Length - 1] == ' ') ? dump.Remove(dump.Length - 1) : dump;
+                    string[] column = dump.Split(' ');
+
+                    result += "\t\t{"; 
+                    foreach(string f in column)
+                    {
+                        string[] field = f.Split('/');
+                        result += "{" + field[0] + ", " + ((field[1] == "") ? "NULL" : field[1]) + ", " + ((field.Length < 3) ? "NULL" : field[2]) + "}, ";
+                    }
+
+                    progressBar.Value++;
+                    result += "}," + Environment.NewLine;
+                }
+                result += "\t};" + Environment.NewLine;
+
+                //for 1
+                result += "\tfor (int i = 0; i < (sizeof(faces))/ sizeof(faces[0]); ++i) {" + Environment.NewLine;
+
+                result += "\t\tglBegin(GL_POLYGON);" + Environment.NewLine;
+
+                //for 2
+                result += "\t\tfor (int j = 0; j < (sizeof(faces[0]))/ sizeof(faces[0][0]); ++j) {" + Environment.NewLine;
+
+                result += "\t\t\tif (faces[i][j][0] != NULL) {" + Environment.NewLine;
+
+                if (glTexCoord3f.Checked)
+                {
+                    result += "\t\t\t\tif (faces[i][j][1] != NULL) {" + Environment.NewLine;
+                    result += "\t\t\t\t\tif(tex_coords[faces[i][j][1] - 1][2] == NULL)" + Environment.NewLine;
+                    result += "\t\t\t\t\t\tglTexCoord2f(tex_coords[faces[i][j][1] - 1][0], tex_coords[faces[i][j][1] - 1][1]);" + Environment.NewLine;
+                    result += "\t\t\t\t\telse" + Environment.NewLine;
+                    result += "\t\t\t\t\t\tglTexCoord3fv(tex_coords[faces[i][j][1] - 1]);" + Environment.NewLine;
+                    result += "\t\t\t\t}" + Environment.NewLine;
+                }
+
+                if (glNormal3f.Checked)
+                {
+                    result += "\t\t\t\tif (faces[i][j][2] != NULL) {" + Environment.NewLine;
+                    result += "\t\t\t\t\tglNormal3fv(normals[faces[i][j][2] - 1]);" + Environment.NewLine;
+                    result += "\t\t\t\t}" + Environment.NewLine;
+                }
+
+                result += "\t\t\t\tglVertex3fv(vertices[faces[i][j][0] - 1]);" + Environment.NewLine;
+
+                result += "\t\t\t}" + Environment.NewLine;
+
+                //endfor 2
+                result += "\t\t}" + Environment.NewLine;
+
+                result += "\t\tglEnd();" + Environment.NewLine;
+
+                //endfor 1
+                result += "\t}" + Environment.NewLine;
+
+                result += "}";
+
+                File.WriteAllText(file_name + ".txt", result);
+                progressBar.Value++;
+
+                MessageBox.Show("Selesai!");
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("ngebug :v -> " + ex.ToString());
+            }
+        }
+    }
+}
